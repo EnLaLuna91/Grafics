271ff9aa571444ad5f336076a841eae8ab916c90
deceiver91 <davidgv@openmailbox.org>
2016-04-08 20:14:49 +0200

    phong

--- Prac_1/practica1Base/src/resources/vshaderGouraud.glsl
+++ Prac_1/practica1Base/src/resources/vshaderGouraud.glsl
@@ -1,15 +1,18 @@
 #if __VERSION__<130
-#define IN varying
+#define IN attribute
 #define OUT varying
 #else
 #define IN in
 #define OUT out
 #endif
 
+
 IN vec4 vPosition;
+//IN vec4 vColor;
 IN vec3 vNormal;
 
 OUT vec4 color;
+
 
 /*
  * Strucrura para pasar los matereiales
@@ -38,41 +41,40 @@ struct Light
     float a;
     float b;
     float c;
-    int on;
 };
 uniform Light luz[3];
 
-/*
- * Uniform para la luz ambiente (Global)
- */
 uniform vec3 vLuzAmbiente;
 
-
-/*
- * Función para calcular la atenuación de una luz
- */
 float calculateAtenuation(int i){
+
+    if (luz[i].tipo == 1){
+        return 1.0f;
+    }
 
     float a = luz[i].a;
     float b = luz[i].b;
     float c = luz[i].c;
-    float d = length(luz[i].coordenadas.xyz - vPosition.xyz); // Distancia de la luz respecto al objeto
+    float d = length(luz[i].coordenadas.xyz - vPosition.xyz);
     float powD = pow(d, 2.0f);
     float dividendo = (a * powD) + (b * d) + c;
 
     return 1.0f/dividendo;
 }
 
-/*
- * Función para calcular la parte de phong para cada luz
- */
-vec3 calculatePhong(int i, vec3 L){
+vec3 calculatePhong(int i){
     vec3 vObs = vec3(0.0f,0.0f,10.0f);
     vec3 V = normalize(vObs - vPosition.xyz);  // Posicion Camara
-    vec3 N = vNormal;            // Normal de vertice
-    vec3 H = normalize(V+L);    // Vector medio normalizado
-    float LN = dot(L,N);        // Producto escalar resultante de multiplicar L * N
-    float NH = dot(N,H);        // Producto escalar resultante de multiplicar N * H
+    vec3 N = vNormal;                   // Normal de vertice
+    vec3 L = (luz[i].coordenadas.xyz - vPosition.xyz); // Posicion luz
+
+//    vec3 H = (L+V/length(L+V));                // Vector medio normalizado
+    vec3 H = normalize(V+L);
+    float LN = dot(L,N);                    // Vector resultante de multiplicar L * N
+    float NH = dot(N,H);                    // Vector resultante de multiplicar N * H
+
+    float maxNH = max(NH,0.0);
+    float powMax_Shininess = pow(maxNH, IMaterial.shininess);
 
     vec3 d = (luz[i].difusa * IMaterial.kd) * max(LN,0.0);
     vec3 s = (luz[i].especular * IMaterial.ks) * pow(max(NH,0.0), IMaterial.shininess);
@@ -81,70 +83,38 @@ vec3 calculatePhong(int i, vec3 L){
     return d+s+a;
 }
 
-
-/*
- * Calcula si la posición esta dentro del angulo de la luz
- */
-bool colorSpotLaight(int i){
-
-    vec3 direcc = normalize(luz[i].coordenadas.xyz - vPosition.xyz);
-
-    float angulo = dot(luz[i].direccion.xyz, direcc);
-
-    if (angulo > luz[i].angulo)
-        return true;
-
-    return false;
+vec3 calculateAmbient(int i){
+    vec3 a = (luz[i].ambiental * IMaterial.ka);
+    return a;
 }
-
 
 void main()
 {
-    gl_Position = vPosition;
+  gl_Position = vPosition;
+//  color = vec4(IMaterial.kd[0], IMaterial.kd[1], IMaterial.kd[2], 1.0);
+//  color = vec4(luz[0].difusa[0], luz[0].difusa[1], luz[0].difusa[2], 1.0);
+//  color = vec4(abs(vNormal.x), abs(vNormal.y), abs(vNormal.z), 1.0);
+//  vec3 L = normalize(luz[0].coordenadas.xyz - vPosition.xyz);
+//  color = vec4(abs(L.x), abs(L.y), abs(L.z), 1.0);
 
-    vec3 phong1 = vec3(0.0, 0.0, 0.0);
-    vec3 phong2 = vec3(0.0, 0.0, 0.0);
-    vec3 phong3 = vec3(0.0, 0.0, 0.0);
+//  vec3 phong1 = calculatePhong(0);
+  vec3 phong1 = calculateAtenuation(0) * calculatePhong(0);
+//  vec3 phong2 = calculateAtenuation(1) * calculatePhong(1);
+//  vec3 phong3 = calculateAtenuation(2) * calculatePhong(2);
 
-    // Luz Puntual
-    if (luz[0].on == 1){
-        vec3 L = (luz[0].coordenadas.xyz - vPosition.xyz); // de la luz respecto al objeto
-        phong1 = calculateAtenuation(0) * calculatePhong(0, L);
-    }
+//  vec3 ITotal = calculateAmbient(0) + phong1;
+//  vec3 ITotal = (vLuzAmbiente * IMaterial.ka) + phong1 + phong2 + phong3;
+  vec3 ITotal = calculateAmbient(0) + phong1 ;
+//  if (ITotal.x > 1.0){
+//      ITotal.x = 1.0;
+//  }
+//  if (ITotal.y > 1.0){
+//      ITotal.y = 1.0;
+//  }
+//  if (ITotal.z > 1.0){
+//      ITotal.y = 1.0;
+//  }
 
-    // Luz Direccional
-    if (luz[1].on == 1){
-        vec3 L = (luz[1].direccion.xyz - vPosition.xyz); // de la luz respecto al objeto
-        phong2 = 1.0f * calculatePhong(1, L);
-    }
-
-    // Luz SpotLight
-    if (luz[2].on == 1){
-        vec3 L = (luz[2].direccion.xyz - vPosition.xyz); // de la luz respecto al objeto
-        /* Se calcula con la dirección + anchura */
-
-        if (colorSpotLaight(2))
-            phong3 = calculateAtenuation(2) * calculatePhong(2, L);
-        else
-            phong3 = vLuzAmbiente * IMaterial.ka;
-    }
-
-
-//    vec3 L = (luz[0].coordenadas.xyz - vPosition.xyz); // Posicion luz
-//    vec3 phong1 = calculateAtenuation(0) * calculatePhong(0, L);
-//    vec3 ITotal = (vLuzAmbiente * IMaterial.ka) + phong1;
-
-    vec3 ITotal = (vLuzAmbiente * IMaterial.ka) + (phong1 + phong2 + phong3);
-
-    if (ITotal.x > 1.0){
-        ITotal.x = 1.0;
-    }
-    if (ITotal.y > 1.0){
-        ITotal.y = 1.0;
-    }
-    if (ITotal.z > 1.0){
-        ITotal.y = 1.0;
-    }
-
-    color = vec4(ITotal, 1.0f);
+  color = vec4(ITotal, 1.0f);
+//  color = vec4(phong1, 1.0f);
 }
