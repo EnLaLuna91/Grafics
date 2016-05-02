643939e250f0269e2b5dfd8dd40300c7f78d6bcc
deceiver91 <davidgv@openmailbox.org>
2016-05-02 19:29:42 +0200

    Formula

--- Prac_2/practica2Test/Object.cpp
+++ Prac_2/practica2Test/Object.cpp
@@ -12,8 +12,45 @@ Object::Object(const glm::mat4 &transform, const Material &material):
     material(material)
   {}
 
+Sphere::Sphere(glm::vec3 coor, float rad){
+    centerSphere = coor;
+    radio = rad;
+}
+
 /* TODO: Implementar en el punt 2 */
-bool Sphere::Intersect(const Ray &ray, IntersectInfo &info) const { return -1.0f; }
+bool Sphere::Intersect(const Ray &ray, IntersectInfo &info) const {
+
+
+    float dirOrigCenter = glm::dot( ray.direction, (ray.origin - centerSphere) );
+    float module = glm::pow( glm::length(ray.origin - centerSphere) , 2.0f );
+    float radCuadratic = glm::pow( radio, 2.0f );
+    float cuadratic = glm::sqrt( glm::pow( dirOrigCenter, 2.0f ) - module + radCuadratic );
+    float distPos = -dirOrigCenter + cuadratic;
+    float distNeg = -dirOrigCenter - cuadratic;
+
+    bool ret;
+
+    if (distPos < 0 || distNeg < 0){
+        if (distPos > 0){
+            info.time = distPos;
+            ret =  true;
+        } else if (distNeg > 0){
+            info.time =  distNeg;
+            ret = true;
+        }
+    } else {
+        if (distPos < distNeg){
+            info.time = distPos;
+            ret = true;
+        }
+        else if (distNeg > distPos){
+            info.time = distNeg;
+            ret = true;
+        }
+    }
+
+    return ret;
+}
 // Function glm::dot(x,y) will return the dot product of parameters. (It's the inner product of vectors)
 
 /* TODO: Implementar en el punt 2 */
--- Prac_2/practica2Test/Scene.cpp
+++ Prac_2/practica2Test/Scene.cpp
@@ -5,6 +5,7 @@ Scene::Scene()
     // Afegeix la camera a l'escena
     cam = new Camera();
     // TODO: Cal crear els objectes de l'escena (punt 2 de l'enunciat)
+    objects.push_back(new Sphere(glm::vec3(0,0,0), 1.5));
 
     // TODO: Cal afegir llums a l'escena (punt 4 de l'enunciat)
 
